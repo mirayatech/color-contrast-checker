095935d8733e552d6283aeaada9e7d8f82c75560
〽️iraya <mirayaabrodi@gmail.com>
2024-01-10 19:53:38 +0100

    feat: Add dynamic color customization to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,47 @@
+import { useState } from "react";
+import { HexColorPicker } from "react-colorful";
+
 export default function App() {
-  return <div>Hello</div>;
+  const [textColor, setTextColor] = useState("#333");
+  const [backgroundColor, setBackgroundColor] = useState("#333");
+
+  return (
+    <div>
+      <input
+        type="text"
+        name="text-color"
+        id="text-color"
+        value={textColor}
+        onChange={() => setTextColor(textColor)}
+      />
+
+      <input
+        type="text"
+        name="background-color"
+        id="background-color"
+        value={backgroundColor}
+        onChange={() => setBackgroundColor(backgroundColor)}
+      />
+
+      <HexColorPicker color={backgroundColor} onChange={setBackgroundColor} />
+      <HexColorPicker color={textColor} onChange={setTextColor} />
+
+      <div
+        style={{
+          width: "300px",
+          height: "300px",
+          backgroundColor: backgroundColor,
+          padding: "20px",
+          borderRadius: "10px",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <p style={{ color: textColor }}>
+          Hello world! I'm Miraya, trying to create a color contrast checker
+        </p>
+      </div>
+    </div>
+  );
 }
